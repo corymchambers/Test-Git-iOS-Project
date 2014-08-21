0a2c1646e24fb9d331bfaa22108c622abf41401b
Cory Chambers <corytyme@yahoo.com>
2014-08-21 08:35:01 -0400

    Initial Commit

---
+++ Git Test/CCViewController.h
@@ -0,0 +1,13 @@
+//
+//  CCViewController.h
+//  Git Test
+//
+//  Created by Cory Chambers on 8/21/14.
+//  Copyright (c) 2014 NA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CCViewController : UIViewController
+
+@end
---
+++ Git Test/CCViewController.m
@@ -0,0 +1,29 @@
+//
+//  CCViewController.m
+//  Git Test
+//
+//  Created by Cory Chambers on 8/21/14.
+//  Copyright (c) 2014 NA. All rights reserved.
+//
+
+#import "CCViewController.h"
+
+@interface CCViewController ()
+
+@end
+
+@implementation CCViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
